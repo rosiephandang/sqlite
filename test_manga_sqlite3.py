c3c2070f4af6a25012fb7b34e73608e82273295a
rosiephandang <23598@burnside.school.nz>
2025-04-11 11:38:55 +1200

    added extra notes and adjust ouput

--- test_manga_sqlite3.py
+++ test_manga_sqlite3.py
@@ -1,18 +1,29 @@
+# docstring - name and application 
+# imports
 import sqlite3
 
+# constants and variables
+database = 'fave_manga_draft.db'
+
+# functions
 def print_manga_faves():
+    # docstring - print all outputs nicely
     fave = input('Rating lower limit: ')
-    db = sqlite3.connect('fave_manga_draft.db')
+    db = sqlite3.connect(database)
     cursor = db.cursor()
     sql = 'SELECT * FROM manga_faves WHERE manga_rating >= ? ORDER BY manga_rating DESC;'
     cursor.execute(sql,(fave,))
     results = cursor.fetchall()
     plus = 1
+    # loop through the results
+    print(f'My current mangas with a rating of {fave} and higher are:')
     for manga in results:
-        print(f'Out of my highest rated mangas currently, at rank {plus}, is {manga[1]}, with a rating of {manga[5]}.')
+        print(f'Rank {plus :<3}{manga[1]:<30}rating: {manga[5]}')
         plus += 1
+    # finish loop here
     db.close
 
+# main codes
 if __name__ == '__main__':
     print_manga_faves()
 
