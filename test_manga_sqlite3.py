88c6284a526a8b0f746f507d317e9b4831c07dd0
rosiephandang <23598@burnside.school.nz>
2025-04-10 14:19:33 +1200

    first commit

---
+++ test_manga_sqlite3.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+def print_manga_faves():
+    fave = input('Rating lower limit: ')
+    db = sqlite3.connect('fave_manga_draft.db')
+    cursor = db.cursor()
+    sql = 'SELECT * FROM manga_faves WHERE manga_rating >= ? ORDER BY manga_rating DESC;'
+    cursor.execute(sql,(fave,))
+    results = cursor.fetchall()
+    plus = 1
+    for manga in results:
+        print(f'Out of my highest rated mangas currently, at rank {plus}, is {manga[1]}, with a rating of {manga[5]}.')
+        plus += 1
+    db.close
+
+if __name__ == '__main__':
+    print_manga_faves()
+
